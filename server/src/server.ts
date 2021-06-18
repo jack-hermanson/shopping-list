3c8c0a802effe1f8303da8a0cdb73b0cd724c32e
Jack Hermanson <jack.hermanson@live.com>
2021-06-18 13:27:07 -0600

    start server

---
+++ server/src/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import * as http from "http";
+import * as socketio from "socket.io";
+import path from "path";
+import { config } from "dotenv";
+import { ConnectionOptions, createConnection } from "typeorm";
+import sslRedirect from "heroku-ssl-redirect";
+
+// env
+const envPath = path.join(__dirname, ".env");
+config({ path: envPath });
+
+// express server
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("port", process.env.PORT || 5000);
+
+// ssl
+console.log(`Environment: ${app.get("env")}`);
+app.use(sslRedirect(["production"]));
+
+// static
+// todo
+
+// routes
+// todo
+
+// database
+/// todo
+
+// http server and socket
+const server = http.createServer(app);
+const io = new socketio.Server({
+    cors: {
+        origin: "*",
+    },
+});
+io.attach(server);
+app.set("socketio", io);
+
+// listen
+server.listen(app.get("port"), () => {
+    console.log(`Server is listening on port ${app.get("port")}.`);
+});
