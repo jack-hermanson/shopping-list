30d82ea0e776e61b37ade1345d4194e3b8435a42
Jack Hermanson <jack.hermanson@live.com>
2021-06-19 14:08:28 -0600

    add footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,14 @@ import "./css/main.css";
 import { Alerts } from "./components/Alerts/Alerts";
 import { Layout } from "./components/Layout/Layout";
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { getVersionNumber } from "./utils/functions";
 
 function App() {
+    useEffect(() => {
+        getVersionNumber();
+    });
+
     return (
         <BrowserRouter>
             <Layout>
--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -1,12 +1,29 @@
 import { FC } from "react";
-import { Container } from "reactstrap";
-import { CONTAINER_FLUID } from "../../utils/constants";
+import { Col, Container, Row } from "reactstrap";
+import { CONTAINER_FLUID, APP_NAME } from "../../utils/constants";
+import { getVersionNumber } from "../../utils/functions";
+import { Link } from "react-router-dom";
 
 export const Footer: FC = () => {
     return (
         <div className="footer-container bg-secondary">
-            <Container className="pb-3 pt-2 mt-2 px-0" fluid={CONTAINER_FLUID}>
-                <span>Test</span>
+            <Container className="pb-3 pt-2 mt-2" fluid={CONTAINER_FLUID}>
+                <Row>
+                    <Col lg={6} className="text-muted">
+                        <Link className="text-light me-3" to="/">
+                            Home
+                        </Link>
+                        <Link className="text-light" to="/account">
+                            Account
+                        </Link>
+                    </Col>
+                    <Col lg={6} className="text-muted d-flex">
+                        <span className="ms-lg-auto">
+                            {APP_NAME} v{getVersionNumber()}&nbsp;[
+                            {process.env.NODE_ENV}]
+                        </span>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
