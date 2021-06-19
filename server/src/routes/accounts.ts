84a4b041de8facbb4896877b82545533b6f2bd7b
Jack Hermanson <jack.hermanson@live.com>
2021-06-18 21:21:17 -0600

    create account

--- server/src/routes/accounts.ts
+++ server/src/routes/accounts.ts
@@ -1,16 +1,37 @@
-import express, { Response } from "express";
+import express, { Response, Request as NormalRequest } from "express";
 import { Request } from "../utils/Request";
 import { AccountService } from "../services/AccountService";
 import { auth } from "../middleware/auth";
+import { LoginOrNewAccountRequest } from "../../../shared/resource_models/account";
+import { validateRequest } from "jack-hermanson-ts-utils/lib/functions/validation";
+import { newAccountSchema } from "../models/Account";
+import { sendError } from "jack-hermanson-ts-utils/lib/functions/errors";
+import { Socket } from "socket.io";
+import { HTTP } from "jack-hermanson-ts-utils";
 
 export const router = express.Router();
 
 // get accounts
-router.get("/", auth, async (req: Request<any>, res: Response) => {
+router.get("/", async (req: Request<any>, res: Response) => {
     res.json(await AccountService.getAll());
 });
 
 // new account
-router.post("/", async (req: Request<any>, res: Response) => {
-    res.json({ test: true });
-});
+router.post(
+    "/",
+    async (req: Request<LoginOrNewAccountRequest>, res: Response) => {
+        try {
+            if (!(await validateRequest(newAccountSchema, req, res))) return;
+
+            const requestBody: LoginOrNewAccountRequest = req.body;
+            const newAccount = await AccountService.create(requestBody, res);
+
+            if (!newAccount) return;
+            delete newAccount.password;
+
+            res.status(HTTP.CREATED).json(newAccount);
+        } catch (error) {
+            sendError(error, res);
+        }
+    }
+);
--- server/src/services/AccountService.ts
+++ server/src/services/AccountService.ts
@@ -1,6 +1,10 @@
 import { Account } from "../models/Account";
 import { getConnection, Repository } from "typeorm";
 import { LoginOrNewAccountRequest } from "../../../shared/resource_models/account";
+import { doesNotConflict } from "jack-hermanson-ts-utils/lib/functions/validation";
+import { Response } from "express";
+import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcryptjs";
 
 const getRepos = (): {
     accountRepo: Repository<Account>;
@@ -11,7 +15,6 @@ const getRepos = (): {
 };
 
 export abstract class AccountService {
-
     // get existing accounts
     static async getAll(): Promise<Account[]> {
         const { accountRepo } = getRepos();
@@ -19,7 +22,31 @@ export abstract class AccountService {
     }
 
     // create new account
-    static async create(newAccount: LoginOrNewAccountRequest) {
+    static async create(
+        newAccount: LoginOrNewAccountRequest,
+        res: Response
+    ): Promise<Account | undefined> {
+        const { accountRepo } = getRepos();
 
+        // check for existing
+        newAccount.username = newAccount.username.toLowerCase();
+        if (
+            !(await doesNotConflict({
+                repo: accountRepo,
+                properties: [{ name: "username", value: newAccount.username }],
+                res,
+            }))
+        ) {
+            return undefined;
+        }
+
+        // create the account
+        const salt = await bcrypt.genSalt(10);
+
+        const account = new Account();
+        account.username = newAccount.username;
+        account.password = await bcrypt.hash(newAccount.password, salt);
+
+        return await accountRepo.save(account);
     }
 }
