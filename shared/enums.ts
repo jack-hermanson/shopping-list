e298e058da02e38fc08a19f81340aee393566f8e
Jack Hermanson <jack.hermanson@live.com>
2021-06-20 14:03:09 -0600

    update shared

--- shared/enums.ts
+++ shared/enums.ts
@@ -1,1 +1,8 @@
 export enum SocketEvent {}
+
+export enum Clearance {
+    NONE = 0,
+    NORMAL = 1,
+    ADMIN = 2,
+    SUPER_ADMIN = 5,
+}
--- shared/resource_models/account.ts
+++ shared/resource_models/account.ts
@@ -1,8 +1,10 @@
 import { ResourceModel } from "./_base";
+import { Clearance } from "../enums";
 
 export interface AccountRecord extends ResourceModel {
     username: string;
     token?: string;
+    clearance: Clearance;
 }
 
 export interface LoginOrNewAccountRequest {
