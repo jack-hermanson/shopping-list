091ab44aa842569efc04d2af9e31f039b4f38d2a
Jack Hermanson <jack.hermanson@live.com>
2021-06-28 21:50:43 -0600

    copy token

--- client/src/pages/Account/Account.tsx
+++ client/src/pages/Account/Account.tsx
@@ -3,6 +3,7 @@ import { Button, Col, Row } from "reactstrap";
 import { useProtectedRoute } from "../../utils/hooks";
 import { useStoreActions, useStoreState } from "../../store";
 import { useHistory } from "react-router-dom";
+import { getToken } from "../../utils/tokens";
 
 export const Account: FC = () => {
     const logOut = useStoreActions(actions => actions.logOut);
@@ -32,6 +33,22 @@ export const Account: FC = () => {
                         }}
                     >
                         Log out
+                    </Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button
+                        className="mt-3"
+                        color="secondary"
+                        onClick={async () => {
+                            const token = getToken();
+                            await navigator.clipboard.writeText(
+                                token || "No token"
+                            );
+                        }}
+                    >
+                        Copy Token
                     </Button>
                 </Col>
             </Row>
